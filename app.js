283bba429c1535791be4c7b2ffb98cc624b4aa7d
unknown <kunalbunkar0104@gmail.com>
2025-08-11 23:53:27 +0530

    feat: make server and connect socketIo

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http');
-const socketIo = require('socketio');
+const socketIo = require('socket.io');
 
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join( __dirname , 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  }); 
+});
 app.get('/', (req, res) => {
-  res.send('Hello, Kunal!');
+  res.render("index");
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 })
